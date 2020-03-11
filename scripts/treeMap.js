831792475b0f0f55ae68f71106ad7b6a7122294c
Celine L <celine168@gmail.com>
2020-03-11 10:56:26 -0700

    Treemap display and colors

--- scripts/treeMap.js
+++ scripts/treeMap.js
@@ -16,33 +16,12 @@ function TreeMap(svg, data) {
   this.draw = (newData) => {
     console.log('drawing treeMap');
 
-    // filteredData = [];
-    // // dict = {};
-    //
-    // newData.forEach(function(d, i) {
-    //
-    //   // For our purposes, if the state is cancelled or suspended,
-    //   // we consider it a failed, non-successful project
-    //   if (d.state == "canceled" || d.state == "suspended") {
-    //     d.state = "failed";
-    //   }
-    //
-    //
-    // });
-    //
-    //
-    // newData = filteredData;
-
-    // Create a hierarchy of data, based on:
-    // project's state -> main category -> category -> number of projects
-    var hierarchy = d3.nest()
+    var nest = d3.nest()
       .key((d) => d.state)
       .key((d) => d.main_category)
       .key((d) => d.category)
       .rollup((v) => v.length)
       .entries(newData);
-
-    console.log(hierarchy);
 
     var treemap = d3.treemap()
       .size([width, height])
@@ -50,41 +29,24 @@ function TreeMap(svg, data) {
       .round(true);
 
     var root = d3.hierarchy({
-      values: hierarchy
-    }, (d) => d.values);
-    console.log(root);
-    var result = treemap(root);
+      values: nest
+    }, (d) => d.values).sum((d) => d.value).sort((a, b) => b.value - a.value);
 
+    var result = treemap(root);
+    
     var node = svg
       .selectAll(".node")
       .data(root.leaves())
-      .enter()
-      .append("div")
+      .enter().append("rect")
       .attr("class", "node")
-      .style("left", (d) => d.x0 + "px")
-      .style("top", (d) => d.y0 + "px")
-      .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
-      .style("height", (d) => Math.max(0, d.y1 - d.y0 - 1) + "px")
-      .style("background", (d) => color(d.parent.data.name))
-      .text((d) => d.data.name);
+      .attr("x", (d) => d.x0 + "px")
+      .attr("y", (d) => d.y0 + "px")
+      .attr("width", (d) => d.x1 - d.x0 + "px")
+      .attr("height", (d) => d.y1 - d.y0 + "px")
+      .attr("fill", d => { 
+        return categoryColors[d.parent.data.key](d.data.key)
+      });
+
   }
 
 }
-
-// d3.csv("./data/ks-projects-201612.csv", function(error, data) {
-//   if (error) throw error;
-//
-//   data.forEach(function(d, i) {
-//     d.goal = +d.goal;
-//     d.pledged = +d.pledged;
-//     d.backers = +d.backers;
-//     //d.category = d["category "];
-//     d.deadline = new Date(d.deadline)
-//     d.launched = new Date(d.deadline)
-//     d.campaignLength = (d.deadline.getTime() - d.launched.getTime()) / (1000 * 3600 * 24); //number of milliseconds in a day
-//   });
-//
-//
-//   treemap = new TreeMap(d3.select('#tree'), data);
-//   treemap.draw(data);
-// });
