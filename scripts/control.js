da0d80f2b416cdf95ef3cd409aebb1511c1d0be7
Theacalix <Theacalix@users.noreply.github.com>
2020-03-15 13:23:45 -0700

    moved button onclicks into js, fixed height of svg

--- scripts/control.js
+++ scripts/control.js
@@ -1,10 +1,10 @@
 let heatMap = undefined;
 let globalData = undefined;
 
-function initCharts(data){
-	globalData = data;
-	heatMap = new HeatMap(d3.select('#heat'), data);
-	controlChange(data, "launched");
+function initCharts(data) {
+  globalData = data;
+  heatMap = new HeatMap(d3.select('#heat'), data);
+  controlChange(data, "launched");
 }
 
 function controlChange(newData, deadlineOrLaunched) {
@@ -13,19 +13,38 @@ function controlChange(newData, deadlineOrLaunched) {
   heatMap.draw(newData, deadlineOrLaunched);
   //treeMap.draw(newData);
 }
+document.querySelector('#deadline')
+  .addEventListener('click', deadline);
 
-function deadline(){
-	console.log("hello deadline");
-	heatMap.draw(globalData, "deadline");
-	//controlChange(data, "deadline");
-	//Update heatmap visualization
-	//update others as well
+function deadline() {
+  console.log("hello deadline");
+  heatMap.draw(globalData, "deadline");
+  //controlChange(data, "deadline");
+  //Update heatmap visualization
+  //update others as well
+}
+document.querySelector('#launched')
+  .addEventListener('click', launched);
+
+function launched() {
+  console.log("hello lanched");
+  heatMap.draw(globalData, "launched");
+  //controlChange(data, "deadline");
+  //update heatmap visualization
+  //update others as well
 }
 
-function launched(){
-	console.log("hello lanched");
-	heatMap.draw(globalData, "launched");
-	//controlChange(data, "deadline");
-	//update heatmap visualization
-	//update others as well
+document.querySelector('#reset')
+  .addEventListener('click', reset);
+
+function reset() {
+  var rectangles = d3.select("#heat").selectAll("rect");
+  rectangles.attr("opacity", 1);
+  for (var it = 0; it < opacityFlat.length; it++) {
+    opacityFlat[it] = 1;
+  }
+  while (selectedTimes.length != 0) {
+    selectedTimes.pop();
+  }
+  //UPdate other visualizations
 }
